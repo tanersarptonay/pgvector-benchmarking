f0b868bfe905a00b8e2c69560dc56e2fdafadcc7
tanersarptonay <sarptonay813@gmail.com>
2025-02-04 18:43:35 +0100

    Result hatası düzeltildi

--- Client/benchmark_runner.py
+++ Client/benchmark_runner.py
@@ -259,7 +259,7 @@ class BenchmarkRunner:
                         warm_up=warm_up
                     )
                     
-                    if self.results:
+                    if result:
                         self.results.append(result)
                         self.append_result_to_csv(result)
 
