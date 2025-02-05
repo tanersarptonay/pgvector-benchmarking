623929ec122d18b552d8efb418cf80b7d35a6c17
tanersarptonay <sarptonay813@gmail.com>
2025-02-05 05:09:17 +0100

    Deleted log files

--- visualizer.py
+++ visualizer.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_latency_heatmap(df):
+def plot_latency_heatmap_size(df):
     """Generates a heatmap for average latency comparisons across indexing strategies and dataset sizes."""
     plt.figure(figsize=(8, 6))
     df_agg = df.groupby(["dataset_size", "indexing_type"], as_index=False).agg({"avg_latency": "mean"})
@@ -12,6 +12,17 @@ def plot_latency_heatmap(df):
     plt.xlabel("Indexing Strategy")
     plt.ylabel("Dataset Size (Rows)")
     plt.title("Average Latency by Indexing Strategy and Dataset Size")
+    plt.show()
+
+def plot_latency_heatmap_dims(df):
+    """Generates a heatmap for average latency comparisons across indexing strategies and dataset sizes."""
+    plt.figure(figsize=(8, 6))
+    df_agg = df.groupby(["dimension", "indexing_type"], as_index=False).agg({"avg_latency": "mean"})
+    pivot_latency = df_agg.pivot(index="dimension", columns="indexing_type", values="avg_latency")
+    sns.heatmap(pivot_latency, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
+    plt.xlabel("Indexing Strategy")
+    plt.ylabel("Dimensions")
+    plt.title("Average Latency by Indexing Strategy and Dimensions")
     plt.show()
 
 
@@ -39,12 +50,25 @@ def plot_latency_distribution(df):
     plt.show()
 
 
+def plot_latency_distribution_only_index(df):
+    """Generates a boxplot showing latency distributions (p50, p90, p95, p99) for each indexing strategy."""
+    df = df[df["indexing_type"].isin(["IVFFlat", "HNSW"])]
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(data=df, x="indexing_type", y="p99_latency", hue="dataset_size", palette="Set2")
+    plt.xlabel("Indexing Strategy")
+    plt.ylabel("p99 Latency (s)")
+    plt.title("Latency Distribution Across Indexing Strategies")
+    plt.legend(title="Dataset Size")
+    plt.grid(axis="y", linestyle="--", alpha=0.7)
+    plt.show()
+
+
 def plot_throughput_vs_indexing(df, overall_throughput=True):
     """Generates a bar chart comparing query throughput across indexing strategies and dataset sizes.
        If overall_throughput is False, results don't include the time between queries.
     """
     thr = "overall_throughput" if overall_throughput else "throughput"
-    thr_title = "Overall Throughput" if overall_throughput else "Throughput"
+    thr_title = "Throughput" if overall_throughput else "Throughput"
 
     plt.figure(figsize=(10, 6))
     sns.barplot(data=df, x="indexing_type", y=thr, hue="dataset_size", palette="pastel")
@@ -55,9 +79,36 @@ def plot_throughput_vs_indexing(df, overall_throughput=True):
     plt.grid(axis="y", linestyle="--", alpha=0.7)
     plt.show()
 
+def plot_latency_vs_indexing(df):
+    """Generates a bar chart comparing query throughput across indexing strategies and dataset sizes.
+       If overall_throughput is False, results don't include the time between queries.
+    """
+    df = df[df["indexing_type"].isin(["IVFFlat", "HNSW"])]
+    plt.figure(figsize=(10, 6))
+    sns.barplot(data=df, x="indexing_type", y="avg_latency", hue="dataset_size", palette="pastel")
+    plt.xlabel("Indexing Strategy")
+    plt.ylabel(f"Latency (sec)")
+    plt.title(f"Latency Across Indexing Strategies")
+    plt.legend(title="Dataset Size")
+    plt.grid(axis="y", linestyle="--", alpha=0.7)
+    plt.show()
 
 def plot_scalability(df):
     """Generates a line chart showing how latency scales with dataset size for each indexing strategy."""
+    df = df[df["indexing_type"].isin(["IVFFlat", "HNSW", "No Index"])]
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(data=df, x="dataset_size", y="avg_latency", hue="indexing_type", marker="o", palette="Dark2")
+    plt.xlabel("Dataset Size (Rows)")
+    plt.ylabel("Average Latency (s)")
+    plt.title("Scalability: Dataset Size vs. Latency Across Indexing Strategies")
+    plt.legend(title="Indexing Strategy")
+    plt.grid(True, linestyle="--", alpha=0.7)
+    plt.show()
+
+
+def plot_scalability_only_index(df):
+    """Generates a line chart showing how latency scales with dataset size for each indexing strategy."""
+    df = df[df["indexing_type"].isin(["IVFFlat", "HNSW"])]
     plt.figure(figsize=(10, 6))
     sns.lineplot(data=df, x="dataset_size", y="avg_latency", hue="indexing_type", marker="o", palette="Dark2")
     plt.xlabel("Dataset Size (Rows)")
@@ -73,7 +124,7 @@ def plot_throughput_vs_dimension(df, overall_throughput=True):
        If overall_throughput is False, results don't include the time between queries.
     """
     thr = "overall_throughput" if overall_throughput else "throughput"
-    thr_title = "Overall Throughput" if overall_throughput else "Throughput"
+    thr_title = "Throughput" if overall_throughput else "Throughput"
     plt.figure(figsize=(10, 6))
     sns.lineplot(data=df, x="dimension", y=thr, hue="indexing_type", marker="o", palette="Dark2")
     plt.xlabel("Embedding Dimensionality")
@@ -89,7 +140,7 @@ def plot_throughput_vs_dataset_size(df, overall_throughput=True):
        If overall_throughput is False, results don't include the time between queries.
     """
     thr = "overall_throughput" if overall_throughput else "throughput"
-    thr_title = "Overall Throughput" if overall_throughput else "Throughput"
+    thr_title = "Throughput" if overall_throughput else "Throughput"
     plt.figure(figsize=(10, 6))
     sns.lineplot(data=df, x="dataset_size", y=thr, hue="indexing_type", marker="o", palette="Set1")
     plt.xlabel("Dataset Size (Rows)")
